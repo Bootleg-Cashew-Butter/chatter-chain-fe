e461172b97527ca7439fd1ee8aad66c82cc6d590
KyleBWilson49 <KyleBWilson49@gmail.com>
2016-05-30 21:18:36 -0700

    dumb components are set up

---
+++ src/app/pods/ActiveUserList/ActiveUserList.component.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import styles from './ActiveUserList.styl'
+
+export default class ActiveUserList extends Component {
+  render () {
+    return <div className={styles.element}>
+      Active User List
+    </div>
+  }
+}
---
+++ src/app/pods/Chat/Chat.component.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import styles from './Chat.styl'
+
+export default class Chat extends Component {
+  render () {
+    return <div className={styles.element}>
+      Chat Box
+    </div>
+  }
+}
